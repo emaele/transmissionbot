9841f9eb14a2fad0283384052b35069779ab7e43
Emanuele Rocco Petrone <emanueleptrn97@gmail.com>
2020-05-31 01:42:40 +0200

    reject non .torrent files

--- handlers/document.go
+++ handlers/document.go
@@ -3,13 +3,21 @@ package handlers
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/emaele/transmissionbot/utility"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/odwrtw/transmission"
 )
 
+// HandleDocument handles incoming files
 func HandleDocument(document tgbotapi.Document, chatid int64, bot *tgbotapi.BotAPI, tc *transmission.Client) {
+
+	if !strings.HasSuffix(document.FileName, ".torrent") {
+		bot.Send(tgbotapi.NewMessage(chatid, "Please send me a valid .torrent file"))
+		return
+	}
+
 	fileURL, err := bot.GetFileDirectURL(document.FileID)
 	if err != nil {
 		log.Println(err)
